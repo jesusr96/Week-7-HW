8950327c097298ad2ab9388612896d34571ed82f
jesusr96 <student@c515-d25hq0ncdhjt-0042012.hostos.cuny.edu>
2017-10-23 19:28:44 -0400

    Raycast Assignment added

---
+++ GD_Week_5/Assets/scripts/CollideAction.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollideAction : MonoBehaviour {
+	public AudioSource myAudio;
+	public AudioClip boomSound;
+	public AudioClip sphereCollide;
+
+	// Use this for initialization
+	void Start () {
+		myAudio = GetComponent<AudioSource>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnCollisionEnter(Collision collisionReport){
+		Debug.Log ("ya done collided");
+		myAudio.PlayOneShot (boomSound, 0.7f);
+
+			if (collisionReport.gameObject.tag == "spheres"){
+			myAudio.PlayOneShot (sphereCollide, 0.7f);
+			}
+	}
+}
---
+++ GD_Week_5/Assets/scripts/seek.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class seek : MonoBehaviour {
+
+	public Transform target;
+	Rigidbody seeker;
+	public float thrust;
+	// Use this for initialization
+	void Start () {
+		seeker = GetComponent<Rigidbody> ();
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 targetDir = Vector3.Normalize(target.position - transform.position);
+		seeker.AddForce (targetDir * thrust);
+
+	}
+}
